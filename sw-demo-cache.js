501299f998ceb50614760ce2311d9485aadf1efd
FishZodiac <leesummer117@gmail.com>
2017-12-05 11:21:16 +0800

    bug

--- sw-demo-cache.js
+++ sw-demo-cache.js
@@ -1,4 +1,4 @@
-var VERSION = 'v1';
+var VERSION = 'v2';
 
 // 缓存
 self.addEventListener('install', function(event) {
@@ -6,7 +6,11 @@ self.addEventListener('install', function(event) {
     caches.open(VERSION).then(function(cache) {
       return cache.addAll([
         './index.html',
-        './static/img/blank.svg'
+        './static/img/blank.svg',
+        './static/css/app.f97aa28bbfdaeffd9452b1ebf5adcd9c.css',
+        './static/js/manifest.45fe879a5a2b55b42025.js',
+        './static/js/vendor.e1811d1b95caa24acd5e.js',
+        './static/js/app.597f674d4b97d9c44cba.js',
       ]);
     })
   );
